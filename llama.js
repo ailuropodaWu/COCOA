c6eb9ee87be5a97d5c6012c486c0c3cd56d78c7a
ailuropodaWu <ailuropoda.cs10@nycu.edu.tw>
2024-03-24 06:44:05 +0800

    Update llama.js

--- llama.js
+++ llama.js
@@ -11,7 +11,7 @@ web3.eth.accounts.wallet.add(SENDER);
 const OAOC_ADDRESS = "0xf59eBE57B47c51f8583264be7e21692fCB211AB4"; // OAOCircle
 // const OAOC_ADDRESS = "0x64BF816c3b90861a489A8eDf3FEA277cE1Fa0E82" // Prompt
 const OAOCircle = new web3.eth.Contract(oaocabi, OAOC_ADDRESS, {from: SENDER.address});
-const TO = 1000000
+const TO = 10000000000
 
 async function waitForTransaction(web3, txHash) {
     let transactionReceipt = await web3.eth.getTransactionReceipt(txHash);
@@ -23,20 +23,31 @@ async function waitForTransaction(web3, txHash) {
 }
 
 async function parseResult(row_data) {
+    console.log(row_data)
     data = row_data.replaceAll('\n', ' ')
     data = data.split(' ')
     return data
 }
 
 async function oao(_instruction) {
-    const condition = "1. from which network 2. to which network 3. from which address 4. to which address 5. amount. answer the questions about the transaction in only 5 words answer! ";
+    const condition =   "1. from which network\n" +  
+                        "2. from which address\n" +
+                        "3. to which network\n" + 
+                        "4. to which address\n" +
+                        "5. transaction amount\n" + 
+                        "Answer about the transaction detail.\n" +
+                        "Give me totaly five words answer!\n";
     // condition = ""
     var prompt = condition.concat(_instruction)
     console.log(`prompt: ${prompt}`)
-    const tx = await OAOCircle.methods.calculateAIResult(prompt).send({value: web3.utils.toWei("0.2", "ether")})
+    const tx = await OAOCircle.methods.calculateAIResult(prompt).send({value: web3.utils.toWei("0.15", "ether")})
     const receipt = await waitForTransaction(web3, tx.transactionHash)
     console.log(receipt)
     const result = await OAOCircle.methods.getAIResult(prompt).call()
     console.log(`result:\n${result}`)
-    return parseResult(result.toString)
+    return parseResult(result)
 }
+
+instrution = "I want to send 40 usdc to the address 0xaa on op testnet using my sepolia address 0xfe44"
+result = oao(instrution)
+
