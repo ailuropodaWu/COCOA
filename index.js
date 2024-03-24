1170f3c57d32458594a19d0fd7b2021a9fef4b32
ailuropodaWu <ailuropoda.cs10@nycu.edu.tw>
2024-03-24 09:00:32 +0800

    modify timeout to avoid long waiting

--- index.js
+++ index.js
@@ -135,7 +135,7 @@ async function transfer(fromNet, fromAddr, toNet, toAddr, amount){
     while(attestationResponse.status != 'complete') {
         const response = await fetch(`https://iris-api-sandbox.circle.com/v1/attestations/${messageHash}`);
         attestationResponse = await response.json()
-        await new Promise(r => setTimeout(r, TO));
+        await new Promise(r => setTimeout(r, 10000));
     }
 
     const attestationSignature = attestationResponse.attestation;
